ae22599a5f494dcd788b13188f171643d58d5928
Deepanshu Kesharwani <dkesharwani2002@gmail.com>
2025-05-13 18:32:50 +0530

    Added Redis integration

--- backend/redis_setup.py
+++ backend/redis_setup.py
@@ -4,7 +4,7 @@ import redis
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT")),
-    password=os.getenv("REDIS_PASSWORD"),
+    # password=os.getenv("REDIS_PASSWORD"),
     decode_responses=True,
     ssl=False  # VERY important since it's using `rediss://`
 )
